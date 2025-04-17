b3bcda54eeee8ead0d93587f44a08ce97ec4c693
ParkJeongSu <pjs91_marrow@naver.com>
2025-04-17 20:35:50 +0900

    deploy config Success

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 🔥 file:// 경로에서 상대경로로 불러오기 위해 꼭 필요!
+  build: {
+    outDir: 'dist' // 👉 기본값이라 생략해도 되지만 명시적으로 써도 OK
+  },
   plugins: [
     vue(),
     vueDevTools(),
